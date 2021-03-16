e86b6f8017f572c9eb29ba111fa90f94f7a83066
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-16 17:05:13 +0800

    7.2.mock数据:2.无法获取真实文件中的sum方法

--- 6.ajax.test.js
+++ 6.ajax.test.js
@@ -1,10 +1,14 @@
 // 用 __mocks__/6.ajax.js 替换掉真实的 6.ajax.js 文件
 jest.mock("./src/6.ajax.js");
 // 这个文件用假的 找同名的文件夹 去替换
-import { fetchUser } from "./src/6.ajax"; // 这里实际上引入的是 __mocks__/6.ajax.js 文件
+import { fetchUser, sum } from "./src/6.ajax"; // 这里实际上引入的是 __mocks__/6.ajax.js 文件
 
 // mock的文件自动会找 __mocks__
 it("测试能否获取用户", async () => {
   let r = await fetchUser();
   expect(r).toEqual({ name: "zf" });
+});
+// TypeError: (0 , _.sum) is not a function
+it("测试求和函数", () => {
+  expect(sum(1, 1)).toBe(2);
 });
--- src/6.ajax.js
+++ src/6.ajax.js
@@ -3,3 +3,6 @@ import axios from "axios";
 export const fetchUser = () => {
   return axios.get("/user");
 };
+export const sum = (a, b) => {
+  return a + b;
+};

14217424f5f29d340f809d60ef40e0fd71854d21
Cherish0207 <cheshire_1207@foxmail.com>
2021-03-16 17:07:40 +0800

    7.2.mock数据:3.requireActual获取真实文件中的sum方法

--- 6.ajax.test.js
+++ 6.ajax.test.js
@@ -1,8 +1,8 @@
 // 用 __mocks__/6.ajax.js 替换掉真实的 6.ajax.js 文件
 jest.mock("./src/6.ajax.js");
 // 这个文件用假的 找同名的文件夹 去替换
-import { fetchUser, sum } from "./src/6.ajax"; // 这里实际上引入的是 __mocks__/6.ajax.js 文件
-
+import { fetchUser } from "./src/6.ajax"; // 这里实际上引入的是 __mocks__/6.ajax.js 文件
+let { sum } = jest.requireActual("./src/6.ajax");
 // mock的文件自动会找 __mocks__
 it("测试能否获取用户", async () => {
   let r = await fetchUser();
